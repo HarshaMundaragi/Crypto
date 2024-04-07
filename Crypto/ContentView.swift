205663ab5f2159588f1296a89bb34bcc13ffa20b
Harsha R Mundaragi <harsha.m@artiumacademy.com>
2024-04-07 14:21:57 +0530

    first

---
+++ Crypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypto
+//
+//  Created by Harsha R Mundaragi on 07/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Harsha R Mundaragi on 07/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
